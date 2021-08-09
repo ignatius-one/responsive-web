72c28bbfa2cf6803ddb2223e0e4fd3f5ee9697e8
ignatius-one <josegnacio118@gmail.com>
2021-08-09 13:20:30 -0500

    ñ

--- scrip/scrip.js
+++ scrip/scrip.js
@@ -5,18 +5,30 @@ function showmenu(){
     document.getElementById('move-content').classList.toggle('moves')
     document.getElementById('menu-show').classList.toggle('show-menu')
 }
+// top up 
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.btn-top-container')
+            .classList.add('show-top')
+    } else {
+          document.querySelector('.btn-top-container')
+            .classList.remove('show-top')
+    }
+}
+document.querySelector('.btn-top-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior:'smooth',
+    })
+})
 
 
-
-
-// code search 
-
+//code search this code should be the final js code because it doesn't run any code down
 
 // executed code 
-
 document.getElementById('ctn-icon-search').addEventListener('click', show_search);
 document.getElementById('cover-ctn-search').addEventListener('click', close_search);
-
 // function search 
 
 bars_search = document.getElementById('ctn-bars-search');
@@ -25,22 +37,14 @@ box_search = document.getElementById('Box-search');
 cover_search = document.getElementById('cover-ctn-search');
 
 // function-show-search 
-
 function show_search() {
     bars_search.style.top = '5em';
     cover_search.style.display = 'block';
     input_search.focus();
-
           if (inputSearch.value === ""){
                 box_search.style.display = "none";
             }
 }
-
-
-
-
-
-
 // function to hidden search 
 function close_search() {
     bars_search.style.top = '-10px';
@@ -49,12 +53,7 @@ function close_search() {
     input_search.value = '';
     box_search.style.display = 'none';
 }
-
-
-
-
 // escape 
-
 function press_scape() {
     teclas_scape = event.keyCode;
     // code 27 is escape  
@@ -65,44 +64,27 @@ function press_scape() {
 window.onkeydown = press_scape;
 // escape code end 
 
-
-
-
 // whent to writing on input
 document.getElementById('inputSearch').addEventListener('keyup',search_inside)
 
-
 // create filtre search 
-
 function search_inside(){
-
-
-    filter = input_search.value.toUpperCase();
-    li = box_search.getElementsByTagName("li");
-
+   let filter = input_search.value.toUpperCase();
+  let  li = box_search.getElementsByTagName("li");
     //filtre all tag "li"
-    for (i = 0; i < li.length; i++){
-
-        a = li[i].getElementsByTagName("a")[0];
-        textValue = a.textContent || a.innerText;
-
+    for (let i = 0; i < li.length; i++){
+       let a = li[i].getElementsByTagName("a")[0];
+      let  textValue = a.textContent || a.innerText;
         if(textValue.toUpperCase().indexOf(filter) > -1){
-
             li[i].style.display = "";
             box_search.style.display = "block";
-
             if (inputSearch.value === ""){
                 box_search.style.display = "none";
             }
-
         }else{
             li[i].style.display = "none";
         }
-
     }
-
-
-
 }
 
 
